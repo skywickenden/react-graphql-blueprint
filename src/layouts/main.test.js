d3745dd19077f361ea7373f03abc493911d1ad35
Sky Wickenden <skywickenden@users.noreply.github.com>
2019-06-18 14:37:21 +0100

    Improved CSS tests

--- src/layouts/main.test.js
+++ src/layouts/main.test.js
@@ -12,11 +12,23 @@ it("Renders and has text content", () => {
 
   // Only need to test elements with styles attached
   // All HTML is tested by the snapshot
-  const titleContent = getByText("React GraphQL Blueprint");
+  const title = getByText("React GraphQL Blueprint");
 
-  const { critical, other } = collect(titleContent, css);
-  expect(critical).toEqual(expect.stringContaining("color:#872258;"));
-  expect(critical).toEqual(expect.stringContaining("font-size:30px;"));
+  const { critical: titleCSS } = collect(title, css);
+  expect(titleCSS).toEqual(expect.stringContaining("color:#872258;"));
+  expect(titleCSS).toEqual(expect.stringContaining("font-size:30px;"));
+
+  const homeLink = getByText("Home");
+  const { critical: homeLinkCSS } = collect(homeLink, css);
+  expect(homeLinkCSS).toEqual(expect.stringContaining("margin:2px 5px;"));
+
+  const fooLink = getByText("Foo");
+  const { critical: fooLinkCSS } = collect(fooLink, css);
+  expect(fooLinkCSS).toEqual(expect.stringContaining("margin:2px 5px;"));
+
+  const { critical: navCSS } = collect(homeLink.parent, css);
+  expect(navCSS).toEqual(expect.stringContaining("background-color:#eeeeff;"));
+  expect(navCSS).toEqual(expect.stringContaining("padding:5px;"));
 
   expect(asFragment()).toMatchSnapshot();
 });
